4b1e57e27578d5850d6408d5df2069f8d4c60a7a
gagagithub <firefox.cui@gmail.com>
2014-10-13 17:43:07 +0800

    bootstrap implement

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -23,7 +23,7 @@ class SessionsController < ApplicationController
 #  		end
 		user = User.where(email: params[:email]).first
 		if user && user.authenticate(params[:password])
-			sign_in user
+#			sign_in user
 			redirect_to user
 		else
 			flash.now[:error] = 'Invalid email/password combination'
