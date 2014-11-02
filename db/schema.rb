4787f397e149f38bc54a02ffe6cde79accc259d9
gagagithub <firefox.cui@gmail.com>
2014-11-02 22:10:50 +0800

    美化完毕

--- db/schema.rb
+++ db/schema.rb
@@ -46,15 +46,6 @@ ActiveRecord::Schema.define(version: 20141029021559) do
     t.datetime "updated_at"
   end
 
-  create_table "microposts", force: true do |t|
-    t.string   "content"
-    t.integer  "user_id"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  add_index "microposts", ["user_id", "created_at"], name: "index_microposts_on_user_id_and_created_at"
-
   create_table "user_ideaships", force: true do |t|
     t.integer  "user_id"
     t.integer  "idea_id"
