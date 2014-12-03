ca6df00422b6aab33ed3ad3eee3feae28d682908
gagagithub <firefox.cui@gmail.com>
2014-12-03 09:54:48 +0800

    添加股东光荣榜

--- app/helpers/ideas_helper.rb
+++ app/helpers/ideas_helper.rb
@@ -8,5 +8,10 @@ module IdeasHelper
     def ideafollowers(idea)
       @ideafollowers = idea.user_ideaships.where(relationtype:1)
     end
+
+  # 返回idea的投资人.
+    def ideastakeholders(idea)
+      @ideastakeholders = idea.user_ideaships.where(relationtype:1,p1donate:300)
+    end
     
 end
