7d5a0cde6b8a6636b4be4985a07da8d5f5345bbc
gagagithub <firefox.cui@gmail.com>
2015-01-19 14:53:03 +0800

    微信功能echo

--- app/controllers/weixin_controller.rb
+++ app/controllers/weixin_controller.rb
@@ -1,11 +1,21 @@
 class WeixinController < ApplicationController
-#  skip_before_filter :verify_authenticity_token
-#  before_filter :check_weixin_legality
+  skip_before_filter :verify_authenticity_token
+  before_filter :check_weixin_legality
   def show
-#  	render :text => params[:echostr]
+  	render :text => params[:echostr]
   end
 
   def create
-#  	if params[:xml][MsgType] == "text"
+  	if params[:xml][MsgType] == "text"
+  		render "echo", :formats => :xml
+  	end 
   end
+
+  private
+  # 根据参数校验请求是否合法，如果非法返回错误页面
+  def check_weixin_legality
+    array = ["productgaga1979", params[:timestamp], params[:nonce]].sort
+    render :text => "Forbidden", :status => 403 if params[:signature] != Digest::SHA1.hexdigest(array.join)
+  end
+
 end
