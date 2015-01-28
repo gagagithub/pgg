898d9f64b74a85c364843efa59e7478ba5c83eca
gagagithub <firefox.cui@gmail.com>
2015-01-28 17:30:31 +0800

    few bug fix 6

--- app/helpers/ideas_helper.rb
+++ app/helpers/ideas_helper.rb
@@ -12,6 +12,9 @@ module IdeasHelper
     idea.user_ideaships.where(user_id:user.id, relationtype:1 ,p1donate:300)
   end
 
+  def user_is_ideaseniorparter(idea,user)
+    idea.user_ideaships.where(user_id:user.id, relationtype:1 ,p2donate:3000)
+  end
 
   # 返回idea的follow user.
   def ideafollowers(idea)
@@ -33,7 +36,6 @@ module IdeasHelper
         return 1
       #募集中级合伙人中  
       else
-              
         if (ideaseniorstakeholders(idea).count <10)
           return 2
         else
@@ -41,8 +43,34 @@ module IdeasHelper
           return 3
         end
       end
+  end
+
+  def idea_user_type(idea)
+
+      if(idea_stage(idea)==1)
+        return 1 if (user_is_ideaowner(idea,current_user))
+        return 2 if (user_is_ideaparter(idea,current_user))
+        return 3 if (user_is_ideaseniorparter(idea,current_user))
+        return 4
+      end
+
+      if(idea_stage(idea)==2)
+        return 5 if (user_is_ideaowner(idea,current_user))
+        return 6 if (user_is_ideaparter(idea,current_user))
+        return 7 if (user_is_ideaseniorparter(idea,current_user))
+        return 8
+      end
+
+      if(idea_stage(idea)==3)
+        return 9 if (user_is_ideaowner(idea,current_user))
+        return 10 if (user_is_ideaparter(idea,current_user))
+        return 11 if (user_is_ideaseniorparter(idea,current_user))
+        return 12
+      end
 
   end
+
+
 
     
 end
